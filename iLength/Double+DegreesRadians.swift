c24b8a0cd67aef9cca111d59d3d88370e4cf2174
knagalak <kavya_nagalakunta@yahoo.co.in>
2017-06-06 23:03:59 +0400

    Extension for Double

---
+++ iLength/Double+DegreesRadians.swift
@@ -0,0 +1,18 @@
+//
+//  Double+DegreesRadians.swift
+//  iLength
+//
+//  Created by Kavya Nagalakunta on 6/6/17.
+//  Copyright © 2017 Kavya Nagalakunta. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func toRadians() ->Double {
+        return self * .pi / 180.0
+    }
+    func toDegrees() -> Double {
+        return self * 180.0 / .pi
+    }
+}
